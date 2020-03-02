d80d4a63ced65b9770e77c85ae7b4be184071e73
Лисененков Дмитрий Александрович <lisenenkov.d@SL24.INTERNAL>
2020-03-02 17:33:02 +0300

    -

---
+++ Lesson1/4_quadratic_equation.rb
@@ -0,0 +1,11 @@
+puts "Коэффициенты уравнения a*x^2 + b*x + c = 0, через запятую"
+(a,b,c) = gets.chomp.split(",").map {|x| x.to_f}
+d = b ** 2 - 4 * a * c
+if d < 0 
+  puts "Дискриминант #{d}. Корней нет"
+elsif d == 0 
+  puts "Дискриминант #{d}. Корень #{-b/(2*a)}"
+else
+  sqrtd = Math.sqrt(d)
+  puts "Дискриминант #{d}. Корни #{(b+sqrtd)/-2/a} и #{(b-sqrtd)/-2/a}"
+end
