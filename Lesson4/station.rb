6b1bab47a59f190b58c495342e558c30d770da7e
Лисененков Дмитрий Александрович <lisenenkov.d@SL24.INTERNAL>
2020-03-05 18:28:36 +0300

    1

---
+++ Lesson4/station.rb
@@ -0,0 +1,20 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def accept(train)
+    @trains.push(train)
+  end
+
+  def send(train)
+    trains.delete(train)
+  end
+
+  def trains_by_type(train_type)
+    @trains.select { |train| train.type == train_type }
+  end
+end
---
+++ Lesson4/train.rb
@@ -0,0 +1,74 @@
+class Train
+  attr_reader :number, :railcars, :speed
+
+  def initialize(number)
+    @number = number
+    @railcars = []
+  end
+
+  def accelerate(value = 1)
+    @speed += value
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def hook_railcar
+    @railcar_count += 1 if @speed == 0
+  end
+
+  def unhook_railcar
+    @railcar_count -= 1 if @railcar_count > 0 && @speed == 0
+  end
+
+  def route=(route)
+    current_station&.send(self)
+    @route = route
+    move(route.stations.first)
+  end
+
+  def current_station
+    return nil if @route.nil?
+
+    index = current_station_index
+    return nil if index.nil?
+
+    stations[index]
+  end
+
+  def prev_station
+    index = current_station_index
+    index > 0 ? @route.stations[index - 1] : nil
+  end
+
+  def next_station
+    index = current_station_index
+    index + 1 < @route.stations.size ? @route.stations[index + 1] : nil
+  end
+
+  def move_forward
+    move(next_station)
+  end
+
+  def move_backward
+    move(prev_station)
+  end
+
+  private
+
+  def current_station_index
+    return nil if @route.nil?
+
+    @route.stations.index { |station| station.trains.include?(self) }
+  end
+
+  def move(to)
+    unless to.nil?
+      @current_station.send(self)
+      @current_station = to
+      @current_station.accept(self)
+    end
+    @current_station
+  end
+end
