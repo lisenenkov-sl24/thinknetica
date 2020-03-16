97c4fc76fc01680a120ff153bc42f825fcca3ba3
Лисененков Дмитрий Александрович <lisenenkov.d@SL24.INTERNAL>
2020-03-16 11:05:23 +0300

    moved when in case to methods

--- Lesson9/modules/validation.rb
+++ Lesson9/modules/validation.rb
@@ -5,11 +5,11 @@ module Validation
     @validations[attr_name].push(
       case validation_type
       when :presence
-        ->(attr_value) { raise "Значение #{name} не заполнено" if !attr_value || attr_value == '' }
+        validate_presence(name)
       when :format
-        ->(attr_value) { raise "Значение #{name} не соответствует формату #{param}" if attr_value !~ param }
+        validate_format(name, param)
       when :type
-        ->(attr_value) { raise "Значение #{name} должно быть типа #{param}" unless attr_value.is_a? param }
+        validate_type(name, param)
       else
         raise 'Неизвестный тип валидации'
       end
@@ -18,6 +18,28 @@ module Validation
   end
 
   private
+
+  def validate_presence(name)
+    lambda do |attr_value|
+      raise "Значение #{name} не заполнено" if !attr_value || attr_value == ''
+    end
+  end
+
+  def validate_format(name, format)
+    lambda do |attr_value|
+      if attr_value !~ format
+        raise "Значение #{name} не соответствует формату #{format}"
+      end
+    end
+  end
+
+  def validate_type(name, type)
+    lambda do |attr_value|
+      unless attr_value.is_a? type
+        raise "Значение #{name} должно быть типа #{type}"
+      end
+    end
+  end
 
   def define_validate_methods(validations)
     define_method(:validate!) do
